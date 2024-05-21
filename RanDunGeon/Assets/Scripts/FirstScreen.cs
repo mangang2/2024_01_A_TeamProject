92e9e36a722237a6421f3de1fe1c8a6835fa5b5a
mangang2 <qkqxhddl5206@gmail.com>
2024-05-21 12:25:00 +0900

    [Test] 각종 씬 전환 UI 추가

---
+++ RanDunGeon/Assets/Scripts/FirstScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstScreen : MonoBehaviour
+{   public void mainscene()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
