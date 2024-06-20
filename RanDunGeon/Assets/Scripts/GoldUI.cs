0315308a06f9d218bb59c8ea8af0f2ca08317f5d
alphapo05 <alphapo05@gmail.com>
2024-06-21 02:54:35 +0900

    [Fix] 골드 숫자 표기 오류 수정

--- RanDunGeon/Assets/Scripts/GoldUI.cs
+++ RanDunGeon/Assets/Scripts/GoldUI.cs
@@ -21,55 +21,25 @@ public class GoldUI : MonoBehaviour
     private string GoldCheck()
     {
         string returnText;
-        long gold = GM.Gold;                        //1234567
-        long temp1, temp2, temp3, temp4, temp5, temp6;
+        string goldTemp;
+        long gold = GM.Gold;
 
-        returnText = "";
+        goldTemp = gold.ToString();
 
-        temp1 = gold % 1000;                       //567 
-
-        temp2 = (gold - temp1)/1000;              //(1234567 - 567) / 1000 = 1234
-
-        temp3 = temp2 % 1000;                       //234
-
-        temp4 = (temp2 - temp3)/1000;               //(1234 - 234) / 1000 = 1
-
-
-        if (temp4 > 0)
+        if (goldTemp.Length > 9)
         {
-            returnText += temp4 + ",";
-            if (temp3 > 0)
-            {
-                returnText += temp3 + ",";
-            }
-            else
-            {
-                returnText += "000,";
-            }
-
-            if (temp1 > 0)
-            {
-                returnText += temp1 + "G";
-            }
-            else
-            {
-                returnText += "000G";
-            }
+            returnText = goldTemp.Substring(0, goldTemp.Length - 9) + "," + goldTemp.Substring(goldTemp.Length - 9, 3) + "," + goldTemp.Substring(goldTemp.Length - 6, 3) + "," + goldTemp.Substring(goldTemp.Length - 3, 3) + "G";
         }
-        else if (temp3 > 0)
+        else if (goldTemp.Length > 6)
         {
-            returnText += temp3 + ",";
-            if (temp1 > 0)
-            {
-                returnText += temp1 + "G";
-            }
-            else
-            {
-                returnText += "000G";
-            }
+            returnText = goldTemp.Substring(0, goldTemp.Length - 6) + "," + goldTemp.Substring(goldTemp.Length - 6, 3) + "," + goldTemp.Substring(goldTemp.Length - 3, 3) + "G";
+        }
+        else if (goldTemp.Length > 3)
+        {
+            returnText = goldTemp.Substring(0, goldTemp.Length - 3) + "," + goldTemp.Substring(goldTemp.Length - 3, 3) + "G";
         }
         else
-        returnText += temp1 + "G";
+            returnText = goldTemp + "G";
 
         return returnText;
     }
